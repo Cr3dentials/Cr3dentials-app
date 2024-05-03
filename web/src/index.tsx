fc331e7979085593a5ed8a0866e8e21f5c78383d
PeterKwesiAnsah <ansahpeter123@yahoo.com>
2024-05-03 08:48:10 +0000

    fix: clean up entry files

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,17 +2,6 @@ import React from 'react'
 
 import ReactDOM from 'react-dom'
 
-import { FatalErrorBoundary } from '@redwoodjs/web'
-import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
+import App from './App'
 
-import FatalErrorPage from 'src/pages/FatalErrorPage'
-import Routes from 'src/Routes'
-
-ReactDOM.render(
-  <FatalErrorBoundary page={FatalErrorPage}>
-    <RedwoodApolloProvider>
-      <Routes />
-    </RedwoodApolloProvider>
-  </FatalErrorBoundary>,
-  document.getElementById('redwood-app')
-)
+ReactDOM.render(<App />, document.getElementById('redwood-app'))
