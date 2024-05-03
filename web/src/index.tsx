86478257408018a4b9e1242c73bf5bf081f6f4c2
PeterKwesiAnsah <ansahpeter123@yahoo.com>
2024-05-03 08:37:07 +0000

    chore: clean up entry

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -8,18 +8,10 @@ import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 import Routes from 'src/Routes'
 
-import client from './graphql' // Import your Apollo Client configuration
-
-import './scaffold.css'
-
 ReactDOM.render(
   <FatalErrorBoundary page={FatalErrorPage}>
-    <RedwoodApolloProvider client={client}>
-      {/* Wrap your app in ChakraProvider */}
-
-        <Routes />
-
-      {/* End ChakraProvider */}
+    <RedwoodApolloProvider>
+      <Routes />
     </RedwoodApolloProvider>
   </FatalErrorBoundary>,
   document.getElementById('redwood-app')
